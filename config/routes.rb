ab420a5ab1539437badd31f9608bbaf1e43b9875
Rich Marshall <p16189101@my365.dmu.ac.uk>
2019-03-12 18:15:19 +0000

    added routing to allow for the new home page to be added, it includes authentication so only logged in users can view the site

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :shouts
 
-  root "shouts#index"
+
+
+
+  #config/routes.rb
+    authenticated :user do
+      root "shouts#index", as: :root #-> if user is logged in
+    end
+
+    unauthenticated :user do
+      root 'home#index', as: :unauthenticated #-> if user is not logged in
+    end
 
 end
