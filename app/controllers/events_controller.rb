a977e58cf903c35a437b7f51bd7b160eb0da29b0
Rich Marshall <p16189101@my365.dmu.ac.uk>
2019-03-12 18:16:24 +0000

    added functionality for users to own events and made all event pages render nicely with bulma

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order("created_at DESC")
+    @event = Event.new
   end
 
   # GET /events/1
@@ -14,7 +16,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   # GET /events/1/edit
@@ -24,7 +26,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190228113746) do
+ActiveRecord::Schema.define(version: 20190312152255) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190228113746) do
     t.string   "host"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "shouts", force: :cascade do |t|
