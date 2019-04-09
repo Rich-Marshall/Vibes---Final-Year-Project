1327a2ee8f53ef0e8436c5238fec484d3568b823
Rich Marshall <P16189101@LEC-ADMIN.dmu.ac.uk>
2019-04-09 13:57:12 +0100

    cleaned up layouts, made sure only a users own posts can be edited, added description to landing page, made website on profile pages a link

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,2 +1,5 @@
 module ShoutsHelper
+  def shout_author(shout)
+    user_signed_in? && current_user.id == shout.user_id
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320163708) do
+ActiveRecord::Schema.define(version: 20190409113539) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190320163708) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "user_id"
+    t.string   "city"
   end
 
   create_table "shouts", force: :cascade do |t|
